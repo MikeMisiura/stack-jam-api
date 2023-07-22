1fa97c834927e1aca67dc39cb012c968b7a8f5d0
Mike Misiura <mikemisiura@gmail.com>
2023-07-22 15:28:53 -0600

    using build

---
+++ src/models/orderProduct.ts
@@ -0,0 +1,45 @@
+import { Document, Schema, Model, model } from 'mongoose';
+
+interface IOrderProduct extends Document {
+    id: string;
+    productName: string;
+    color: string;
+    description: string;
+    price: number;
+    groupCode: string;
+    featured?: boolean;
+}
+
+const orderProductSchema: Schema = new Schema({
+    productName: {
+        type: String,
+        required: true,
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    groupCode: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+const OrderProduct: Model<IOrderProduct> = model('OrderProduct', orderProductSchema);
+
+export { IOrderProduct, OrderProduct };
